38078b46b382109f847ba18176e962bb584b9d71
unknown <932027769@qq.com>
2023-10-15 23:43:26 +0800

    add

---
+++ transfer-dev/modules/xuewei-ui/index.ts
@@ -0,0 +1,11 @@
+import Transfer from './Transfer/transferSelf.vue'
+const components = {
+    Transfer
+}
+export default {
+    install(app) {
+        for (const comp in components) {
+            app.component(comp, components[comp])
+        }
+    }
+} 
