b7ee20720877514af41094fe56a11a103de730c4
向学伟 <932027769@qq.com>
2024-01-25 09:52:08 +0800

    --

--- JavaScript基础/index/index.js
+++ JavaScript基础/index/index.js
@@ -1,3 +1,15 @@
 (function($) {
     console.log("=jquery=",$);
 })(window.$)
+
+(() => {
+    var defining = {
+        a: function() { },
+        b: function() { },
+        c: function() { }
+    };
+
+    Object.keys(defining).forEach(key => {
+        window[key] = defining[key];
+    });
+})();
