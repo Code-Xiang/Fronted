6432ded011004c6cd35e0da1122365ff0fbc5fe4
XueWei <932027769@qq.com>
2024-01-30 20:27:33 +0800

    commit

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,56 +1,31 @@
 <template>
   <div class="person">
     <h2>当前求和为：{{ sum }}</h2>
-    <button @click="add">点我sum+1</button>
-    <textarea disabled name="" id="text_fn" cols="30" rows="10" style="resize:none;border: none; background: none;" readonly>{{ changeword }}</textarea>
+    <button @click="add">点我</button>
+    <hr>
+    <img v-for="(item,index) of dogList" :src="item" :key="index">
+    <button @click="addDog">再来一只狗</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref,onBeforeMount,onMounted,onBeforeUpdate,onUpdated, onBeforeUnmount, onUnmounted, computed } from 'vue'
+import { ref,reactive } from 'vue'
+import axios from 'axios'
+// https://dog.ceo/api/breed/pembroke/images/random
 let sum = ref(0)
-const add = () => {
-  sum.value += 1
+let dogList = reactive([
+  'https://images.dog.ceo/breeds/pembroke/n02113023_631.jpg',
+  'https://images.dog.ceo/breeds/pembroke/n02113023_7243.jpg',
+  'https://images.dog.ceo/breeds/pembroke/n02113023_14262.jpg'
+])
+function add() {
+  sum.value += 1;
 }
-
-// 创建 beforeCreate created => setup
-console.log('创建');
-const changeword = computed(() => {
-  let str = "请选择下列啊寄快递hi啊图，然后怎么..."
-  let wordsArray = str.split('，');
-  return wordsArray.join('，\n');
-})
-// 挂载
-onBeforeMount(() => {
-  console.log('挂载前');
-})
-computed(()=>{
-
-})
-// 挂载完毕
-onMounted(() => {
-  console.log('子---挂载完毕');
-})
-
-// 更新前
-onBeforeUpdate(()=> {
-  console.log('更新前');
-  
-})
-// 更新完毕
-onUpdated(()=> {
-  console.log('更新完毕');
-})
-
-// 卸载前
-onBeforeUnmount(()=> {
-  console.log('卸载前');
-})
-
-// 卸载完毕
-onUnmounted(() => {
-  console.log('卸载完毕');
-})
+function addDog() {
+  axios.get('https://dog.ceo/api/breed/pembroke/images/random').then(
+    
+  )
+}
 </script>
 <style scoped>
 .person {
@@ -62,5 +37,8 @@ onUnmounted(() => {
 }
 button {
   margin: 0 5px;
+}
+img {
+  height: 100px;
 }
 </style>
